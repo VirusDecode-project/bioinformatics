a30f5bb5aa0756e3f7e5f5e4dabbd14161c6f612
Guho <weeeeek2@naver.com>
2024-07-07 16:57:09 +0900

    feat: Protein mutation 1. 기존 DNA mutation 기능 활용하여 protein mutation 기능 구현

--- level2/protein_mutation/level2.py
+++ level2/protein_mutation/level2.py
@@ -1,36 +1,63 @@
 from Bio.Seq import Seq
-from Bio import pairwise2
-from Bio.pairwise2 import format_alignment
+from Bio.SeqRecord import SeqRecord
+from Bio.Align import MultipleSeqAlignment
+from Bio import AlignIO, SeqIO
+import subprocess
 
-# 예제 DNA 서열 (reference와 variant)
-reference_dna = Seq("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGATAG")
-variant_dna = Seq("ATGGCCATTGTAATGGGCCGCTGAAAGGGTGCCCGA")
+class SequenceAlignment:
+    def __init__(self, files, muscle_exe="muscle"):
+        self.files = files
+        self.muscle_exe = muscle_exe
+        self.combined_file = "combined.fasta"
+        self.aligned_file = "aligned.fasta"
+        self.aligned_dna_file = "aligned_dna.fasta"
+        self.base_colors = {
+            'A': '#ff9999',
+            'T': '#99ff99',
+            'G': '#9999ff',
+            'C': '#ffff99',
+            '-': '#cccccc'
+        }
+        self.sequences = []
+        self.aligned_sequences = []
+        self.protein_sequences = []
+        self.mutations = []
 
-# DNA 서열을 단백질 서열로 번역
-reference_protein = reference_dna.translate()
-variant_protein = variant_dna.translate()
+    def read_sequences(self):
+        self.sequences = [SeqIO.read(file, "fasta") for file in self.files]
 
-print("Reference Protein Sequence:")
-print(reference_protein)
-print("Variant Protein Sequence:")
-print(variant_protein)
+    def combine_sequences(self):
+        with open(self.combined_file, "w") as f:
+            SeqIO.write(self.sequences, f, "fasta")
 
-# 서열 정렬
-alignments = pairwise2.align.globalxx(reference_protein, variant_protein)
+    def run_muscle(self):
+        subprocess.run([self.muscle_exe, "-in", self.combined_file, "-out", self.aligned_file])
 
-# 가장 좋은 정렬 결과 출력
-print("Best Alignment:")
-print(format_alignment(*alignments[0]))
+    def read_alignment(self):
+        alignment = AlignIO.read(self.aligned_file, "fasta")
+        self.aligned_sequences = [record.seq for record in alignment]
 
-# 뮤테이션 찾기
-ref_aligned = alignments[0][0]
-var_aligned = alignments[0][1]
+    def translate_sequences(self):
+        self.protein_sequences = []
+        for dna_seq in self.aligned_sequences:
+            clean_dna_seq = str(dna_seq).replace('-', 'N')
+            clean_dna_seq = Seq(clean_dna_seq)
+            protein_seq = clean_dna_seq.translate()
+            self.protein_sequences.append(protein_seq)
+        for idx, protein_seq in enumerate(self.protein_sequences, start=1):
+            print(f"Translated protein sequence {idx}: {protein_seq}")
 
-mutations = []
-for i in range(len(ref_aligned)):
-    if ref_aligned[i] != var_aligned[i]:
-        mutations.append((i, ref_aligned[i], var_aligned[i]))
+    
 
-print("Mutations found:")
-for (pos, ref, var) in mutations:
-    print(f"Position: {pos}, Reference: {ref}, Variant: {var}")
+    def run(self):
+        self.read_sequences()
+        self.combine_sequences()
+        self.run_muscle()
+        self.read_alignment()
+        self.translate_sequences()
+
+
+if __name__ == "__main__":
+    files = ["NC_045512.fasta", "OX014251.fasta"]
+    alignment = SequenceAlignment(files)
+    alignment.run()
